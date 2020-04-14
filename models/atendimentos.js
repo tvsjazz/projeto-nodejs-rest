e9a6fd5256b869fe09763fdc6abca5e76ec1b38a
Tiago Vieira Siqueira <tiagov@badesc.gov.br>
2020-04-14 14:23:42 -0300

    finalizando data

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -3,9 +3,10 @@ const conexao = require('../infraestrutura/conexao');
 
 class Atendimentos {
     adiciona(atendimento) {
-        const dataCriacao = new Date();
-        const data = moment(atendimento.data).format('YYYY-MM-DD HH:MM:SS');
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
         const atendimentoDatado = {...atendimento, dataCriacao, data};
+        
         const sql = 'INSERT INTO Atendimentos SET ?';
 
         conexao.query(sql, atendimentoDatado, (erro, resultados) => {
