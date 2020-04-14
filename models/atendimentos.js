51620cb053043fe44934cbb98fea1bb253f295ab
Tiago Vieira Siqueira <tiagov@badesc.gov.br>
2020-04-14 14:23:05 -0300

    adicionando data de criação e data de agendamento

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,10 +1,14 @@
+const moment = require('moment');
 const conexao = require('../infraestrutura/conexao');
 
 class Atendimentos {
     adiciona(atendimento) {
+        const dataCriacao = new Date();
+        const data = moment(atendimento.data).format('YYYY-MM-DD HH:MM:SS');
+        const atendimentoDatado = {...atendimento, dataCriacao, data};
         const sql = 'INSERT INTO Atendimentos SET ?';
 
-        conexao.query(sql, atendimento, (erro, resultados) => {
+        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
             if (erro) {
                 console.log(erro);
             } else {
